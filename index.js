6fc4e99b22866e9e47cda0578697eec86ec4bf2c
Aneeka Mangal <aneekamangal@gmail.com>
2023-08-20 12:12:14 +0530

    socket connected

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { Server } = require("socket.io");
 const io = new Server(8000, { cors: true });
 
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
 io.on("connection", (socket) => {
-  console.log("Socket connected", socket.id);
+  console.log(`Socket connected`, socket.id);
   socket.on("room:join", (data) => {
-    console.log(data);
+    // console.log(data);
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(socket.id).emit("room:join", data);
   });
 });
